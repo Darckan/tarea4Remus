08c2f30403024615d100073e05e0c2506a2d7bcd
Darckan <remushurmuz4@gmail.com>
2019-02-08 12:00:22 +0100

    Añadir main ejectuable

---
+++ src/tarea4remus/main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea4remus;
+
+/**
+ *
+ * @author remus
+ */
+public class main {
+    
+    public static void main(String [] args)
+    {
+            Persona Vico = new Persona("vico", "nolose");
+            
+            System.out.println(Vico);
+    }
+}
